2e0f3b6195773b23f3d214b2dc9d1dd44cc5a234
Laryssa Melnyk <laryssamelnyk@Laryssas-MacBook-Pro.local>
2021-08-31 22:22:33 -0400

    Write LDL analysis and driver

--- blood_calculator.py
+++ blood_calculator.py
@@ -4,6 +4,7 @@ def interface():
     while keep_running:
         print("Make a choice")
         print("1 - HDL Analysis")
+        print("2 - LDL Analysis")
         print("9 - Quit")
         choice = int(input("Make a choice: "))
         print(type(choice))
@@ -13,6 +14,11 @@ def interface():
         elif choice == 1:
             HDL_Driver()
         print(choice)
+
+        elif choice == 2:
+            LDL_Driver()
+        print(choice)
+
     return choice
 
 
@@ -36,6 +42,31 @@ def hdl_analysis(HDL_value):
 def hdl_output(HDL_value, HDL_character):
     print("The HDL value of {} is condsidered {}".format(HDL_value, HDL_character))
     return
+
+
+def LDL_Driver():
+    LDL_value = ldl_input()
+    LDL_character = ldl_analysis(LDL_value)
+    ldl_output(LDL_value, LDL_character)
+
+def ldl_input():
+    hdl_value = int(input(("Enter LDL value: ")))
+    return ldl_value
+
+def ldl_analysis(LDL_value):
+    if LDL_value >= 130:
+        return "Normal"
+    elif 130 <= HDL_value < 159:
+        return "Borderline High"
+    elif 160 <= HDL_value < 189:
+        return "High"
+    else:
+        return "Very High"
+
+def ldl_output(LDL_value, LDL_character):
+    print("The LDL value of {} is condsidered {}".format(LDL_value, LDL_character))
+    return  
+
 
 interface()
         
