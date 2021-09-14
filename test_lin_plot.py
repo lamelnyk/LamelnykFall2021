318bbf47ea50db2af74af70cc6a7963df3f4adc8
Laryssa Melnyk <laryssamelnyk@Laryssas-MBP.lan>
2021-09-14 15:40:34 -0400

    added second test statement to test for 3rd Y output

--- test_lin_plot.py
+++ test_lin_plot.py
@@ -1,8 +1,16 @@
 import pytest
 
-@pytest.mark.parametrize("tuple1, tuple2, expected", [
+@pytest.mark.parametrize("list1, list2, expected", [
     ((1,1), (2,2), 1.0)])
-def test_lin_plot(tuple1, tuple2, expected): 
+def test_calculate_slope(list1, list2, expected): 
     from lin_plot import calculate_slope
-    answer = calculate_slope(tuple1, tuple2)
-    assert answer == expected 
+    answer = calculate_slope(list1, list2)
+    assert answer == expected
+
+@pytest.mark.parametrize("tuple1, value3, slope, expected", [
+    ((1,1), 3 , 1.0, 3 )])
+def test_calculate_y3(slope, tuple1, value3, expected): 
+    from lin_plot import calculate_y3
+    answer = calculate_y3(slope, tuple1, value3)
+    assert answer == expected
+    
