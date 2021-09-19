311e85740711e4b26c545ddbabd9004ae4d521c1
Laryssa Melnyk <laryssamelnyk@Laryssas-MacBook-Pro.local>
2021-09-19 00:41:19 -0400

    adding function which takes a weight enthry and converts into kg - class work entry

--- weight_entry.py
+++ weight_entry.py
@@ -8,7 +8,8 @@ def input_weight_entry():
 
 def parse_weight_input(weight_input):
     weight, units = weight_input.split(' ')
-    weight = int(weight)
+    weight = float(weight)
+    units = units.rstrip('s')
     if units == "lb":
         weight_kg = convert_lb_to_kg(weight)
     else:
